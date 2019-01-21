b79b6f7c1d4b848074a521d7cd3faeb1a1875761
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-21 14:30:54 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,24 @@
+class Doctor
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+
+    def appointments
+        Appointment.all.select { |doctor| doctor == self }
+    end
+
+    def patients
+
+    end
+
+
+end
