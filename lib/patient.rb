4b1f7ff01cd047c0ed448c7dc964279c5e7ab538
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-21 14:38:25 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -13,7 +13,7 @@ class Patient
     end
 
     def new_appointment(date, doctor)
-        Appointment.new(date, self, doctor)
+        Appointment.new(self, date, doctor)
     end
 
     def appointments
