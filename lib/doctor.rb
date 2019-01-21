0fb159bea08de5977d495d269c9c1e392b73bde9
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-21 14:32:36 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -8,8 +8,8 @@ class Doctor
         @@all << self
     end
 
-    def self.all 
-        @@all 
+    def self.all
+        @@all
     end
 
     def new_appointment(date, patient)
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,3 +1,11 @@
 class Patient
+    attr_accessor :name 
+
+    @@all = [] 
+
+    def initialize(name)
+        @name = name 
+        @@all << self 
+    end
 
 end
