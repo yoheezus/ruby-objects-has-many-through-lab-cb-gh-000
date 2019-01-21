7766dcb8ec2c98c14ee1641b4d5c2b802952f87a
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-21 14:41:51 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,7 +17,7 @@ class Patient
     end
 
     def appointments
-        Appointment.all.select { |patient| patient == self }
+        Appointment.all.select { |app| app.patient == self }
     end
 
     def doctors
